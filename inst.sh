448b2f7b665e6a62949f36108d0ca82e5e6a930a
Hirosuke Miyajima (宮島大介) <hirosuke.miyajima@gmail.com>
2026-04-19 01:30:09 +0900

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 sudo apt-get update
+sudo apt-get install python3-venv pip
 sudo apt-get install ffmpeg gnuplot qiv
 sudo apt-get install imagemagick figlet
 sudo apt-get install wget w3m pup
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox_0.12.6.1-3.bookworm_amd64.deb
 sudo apt install ./wkhtmltox_0.12.6.1-3.bookworm_amd64.deb
-
+cd /home/user
+python3 -m venv py
+source py/bin/activate
+pip install kiwipiepy
