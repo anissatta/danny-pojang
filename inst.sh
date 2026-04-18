c1c76ed7264600c44a4b342a46841ee106883bde
Hirosuke Miyajima (宮島大介) <hirosuke.miyajima@gmail.com>
2026-04-19 02:24:17 +0900

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -8,8 +8,8 @@ yes | sudo apt-get install wget w3m pup
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox_0.12.6.1-3.bookworm_amd64.deb
 sudo apt install ./wkhtmltox_0.12.6.1-3.bookworm_amd64.deb
 cd /home/user
-python3 -m venv /home/user/py
-source /home/user/py/bin/activate
-pip install kiwipiepy
+#python3 -m venv /home/user/py
+#source /home/user/py/bin/activate
+#pip install kiwipiepy
 wget git.io/trans
 chmod +x ./trans
