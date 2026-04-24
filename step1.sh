9201b1aa68ccae801edb2e80f568d642d7ed37b0
Hirosuke Miyajima (宮島大介) <hirosuke.miyajima@gmail.com>
2026-04-24 11:08:24 +0900

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -3,7 +3,7 @@
 # as root, execute this script before starting anything else. 
 yes | apt-get update
 yes | apt-get install python3-venv pip gawk curl python3-feedparser
-yes | apt-get install ffmpeg gnuplot qiv
+yes | apt-get install ffmpeg gnuplot qiv sqlite3
 yes | apt-get install imagemagick figlet
 yes | apt-get install wget w3m pup
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox_0.12.6.1-3.bookworm_amd64.deb
